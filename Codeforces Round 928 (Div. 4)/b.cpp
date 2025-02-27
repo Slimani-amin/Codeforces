7a9b28a371b208d72c0dde7cc403a9cfb3bca2a6
Slimani-amin <amineslimani1610@gmail.com>
2025-02-27 02:14:15 +0000

    Add solution for problem B in Codeforces Round 928

--- Codeforces Round 928 (Div. 4)/b.cpp
+++ Codeforces Round 928 (Div. 4)/b.cpp
@@ -6,6 +6,34 @@ using namespace std;
 
 
 void solve() {
+
+    int n; cin>>n;
+    vector<vector<int>> grid;
+    for(int i = 0; i < n; i++) {
+        vector<int> row;
+        string s; cin>>s;
+        for(int j = 0; j < s.size(); j++) {
+            if(s[j] == '1') row.push_back(1);
+        }
+        if(!row.empty()){
+            grid.push_back(row);
+        }
+    }
+     bool square = true;
+    for(int i =0;i<grid.size();i++){
+        if(grid.size()!=grid[i].size()){
+            square = false;
+            break;
+        }
+    }
+
+    if(square){
+        cout<<"SQUARE"<<endl;
+    }
+    else{
+        cout<<"TRIANGLE"<<endl;
+    }
+
 }
 
 int main() {
