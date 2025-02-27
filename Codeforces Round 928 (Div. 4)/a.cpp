4ababae9fbd3f3aab1c5c21bd74903e227608a81
Slimani-amin <amineslimani1610@gmail.com>
2025-02-27 01:44:22 +0000

    Implement solution for problem A in Codeforces Round 928

--- Codeforces Round 928 (Div. 4)/a.cpp
+++ Codeforces Round 928 (Div. 4)/a.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 
 void solve() {
+    string s; cin>>s;
+    int a,b;
+    a = 0;
+    b = 0;
+    for (int i = 0; i<5;i++){
+        if (s[i] == 'A'){
+            a++;
+        }
+        else{
+            b++;
+        }
+        if(a>=3){
+            cout<<"A"<<endl;
+            break;
+        }
+        if(b>=3){
+            cout<<"B"<<endl;
+            break;
+        }
+    }
 }
 
 int main() {
