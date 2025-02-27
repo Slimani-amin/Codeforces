f0bdf80575928d96226dec2b9e6d4316c0173a10
Slimani-amin <amineslimani1610@gmail.com>
2025-02-27 09:42:16 +0000

    Add solution for problem C in Codeforces Round 928

--- Codeforces Round 928 (Div. 4)/c.cpp
+++ Codeforces Round 928 (Div. 4)/c.cpp
@@ -1,11 +1,35 @@
 #include <bits/stdc++.h>
-
+#define ll long long
 using namespace std;
 
 
 
 
 void solve() {
+    int n; cin>>n;
+    ll ans = 0;
+    ll count =1;
+    ll rest =9;
+    ll k = 11;
+    
+    for(int i = 1; i <= n; i++) {
+        if(i%10 ==0 && (i/10)%9 == 0 && (i/10)%k){
+            rest+=9;
+            k = k*10;
+            k++;
+            
+        }
+
+        ans+=count;
+        count++;
+       if(count==rest+1){
+        count = 1;
+       }
+
+    }
+
+    cout<<ans<<endl;
+
 }
 
 int main() {
