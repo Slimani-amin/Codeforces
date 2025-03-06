50960d545a6653fa7f108fb389778af580b005ad
youssefbouhaous <youssefbouhaous01@gmail.com>
2025-03-06 01:34:48 +0000

    adding the problem e

--- Codeforces Round 886 (Div. 4)/e.cpp
+++ Codeforces Round 886 (Div. 4)/e.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define ll long double
+#define ll unsigned long long
 
 using namespace std;
 
@@ -8,6 +8,36 @@ using namespace std;
 
 
 void solve() {
+    ll n,c;cin>>n>>c;
+    vector<ll>a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    ll l=1;
+    ll r=1e9;
+    ll ans=0;
+    while(l<=r){
+        ll mid=(l+r)/2;
+        ll sum=0;
+        bool flag=false;
+        for(int i=0;i<n;i++){
+            if(sum>c)break;
+            ll b=sum;
+            sum=sum+(a[i]+2LL*mid)*(a[i]+2LL*mid);
+            if(sum<b){
+                flag=true;
+                break;
+            }
+        }
+        if(sum==c){
+            cout<<mid<<endl;
+            return;
+        }
+        if(sum>c){
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
 }
 
 int main() {
